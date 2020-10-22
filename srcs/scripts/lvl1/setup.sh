3eb08fb177d2c4ba592f55206571a27bdc62905f
Bruno Azuara <bruno.azuara@gmail.com>
2020-10-22 13:54:52 +0200

    fixed typo in lvl1 setup script

--- srcs/scripts/lvl1/setup.sh
+++ srcs/scripts/lvl1/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ssh-keygen -q -t rsa -N '' -f /home/enderi_wiggin/.ssh/id_rsa
+ssh-keygen -q -t rsa -N '' -f /home/ender_wiggin/.ssh/id_rsa
 ssh-copy-id ender@bajovnick_1
 ssh-copy-id ender@bajovnick_2
 ssh-copy-id ender@bajovnick_3
