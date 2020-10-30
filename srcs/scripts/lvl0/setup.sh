cc6f61053e543d6f714b1e56a9fabf1445892469
Bruno Azuara <bruno.azuara@gmail.com>
2020-10-30 13:32:59 +0100

    remove ssh warnings and automate ssh key install

--- srcs/scripts/lvl0/setup.sh
+++ srcs/scripts/lvl0/setup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 ssh-keygen -q -t rsa -N '' -f /home/ender_wiggin/.ssh/id_rsa
-ssh-copy-id ender@bajovnick
+echo -e "Host * \n \tStrictHostKeyChecking no" > .ssh/config
+sshpass -p ender ssh-copy-id ender@bajovnick -p 22
+#ssh-copy-id ender@bajovnick
--- srcs/scripts/lvl1/setup.sh
+++ srcs/scripts/lvl1/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ssh-keygen -q -t rsa -N '' -f /home/ender_wiggin/.ssh/id_rsa
-ssh-copy-id ender@bajovnick_1
-ssh-copy-id ender@bajovnick_2
-ssh-copy-id ender@bajovnick_3
+echo -e "Host * \n \tStrictHostKeyChecking no" > .ssh/config
+sshpass -p ender ssh-copy-id ender@bajovnick_1 -p 22
+sshpass -p ender ssh-copy-id ender@bajovnick_2 -p 22
+sshpass -p ender ssh-copy-id ender@bajovnick_3 -p 22
